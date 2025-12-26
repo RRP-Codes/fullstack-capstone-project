60ac6a0c04bc0c61d04be51b59e2c34e5e330f2e
RRP-Codes <ray.peel@live.com>
2025-12-26 16:17:06 -0500

    Finalising Routes

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,74 +1,28 @@
 /*jshint esversion: 8 */
-router.get('/', async (req, res) => {
-    try {
-        // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
-const logger = require('../logger');  // optional, if you have a logger
 
-router.get('/', async (req, res, next) => {
-    logger.info('/api/gifts called');
+// Get all gifts
+router.get('/', async (req, res) => {
     try {
-        const db = await connectToDatabase();               // Task 1: Connect to DB
-    } catch (e) {
-        logger.error('Error fetching gifts', e);
-        next(e);
-    }
-});
-
-module.exports = router;
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
-const collection = db.collection("gifts");          // Task 2: Access collection
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        // const gifts = {{insert code here}}
-const gifts = await collection.find({}).toArray();  // Task 3: Fetch all gifts
-        // Task 4: return the gifts using the res.json method
-        res.json(gifts);                                     // Task 4: Return gifts);
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gifts = await collection.find({}).toArray();
+        res.json(gifts);
     } catch (e) {
         console.error('Error fetching gifts:', e);
         res.status(500).send('Error fetching gifts');
     }
 });
 
+// Get gift by ID
 router.get('/:id', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
-        // const db = {{insert code here}}
-const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
-
-router.get('/:id', async (req, res, next) => {
-    try {
-        const db = await connectToDatabase();               // Task 1
-        const id = req.params.id;
-
-        if (!gift) {
-            return res.status(404).send("Gift not found");
-        }
-
-        res.json(gift);
-    } catch (e) {
-        next(e);
-    }
-});
-
-module.exports = router;
-        // Task 2: use the collection() method to retrieve the gift collection
-        // {{insert code here}}
-const collection = db.collection("gifts");          // Task 2
-        const id = req.params.id;
-
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-        // {{insert code here}}
-const gift = await collection.findOne({ id: id });  // Task 3
-        if (!gift) {
-            return res.status(404).send('Gift not found');
-        }
-
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gift = await collection.findOne({ id: req.params.id });
+        if (!gift) return res.status(404).send('Gift not found');
         res.json(gift);
     } catch (e) {
         console.error('Error fetching gift:', e);
@@ -76,18 +30,16 @@ const gift = await collection.findOne({ id: id });  // Task 3
     }
 });
 
-
-
 // Add a new gift
-router.post('/', async (req, res, next) => {
+router.post('/', async (req, res) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
-        const gift = await collection.insertOne(req.body);
-
-        res.status(201).json(gift.ops[0]);
+        const result = await collection.insertOne(req.body);
+        res.status(201).json(result.ops[0]);
     } catch (e) {
-        next(e);
+        console.error('Error adding gift:', e);
+        res.status(500).send('Error adding gift');
     }
 });
 
