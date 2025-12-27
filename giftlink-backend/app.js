a7efb7490fe0b3145d2547e2ef78e1e270edcb51
RRP-Codes <ray.peel@live.com>
2025-12-27 09:24:29 -0500

    Backend register Component

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -8,46 +8,33 @@ const connectToDatabase = require('./models/db');
 const { loadData } = require('./util/import-mongo');
 
 const app = express();
-app.use("*", cors());
+app.use("*",cors());
 const port = 3060;
-
-// Connect to MongoDB; then load data
-connectToDatabase().then(async () => {
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
-
-    // Load initial gift data safely
-    try {
-        await loadData();
-    } catch (err) {
-        console.error("Failed to load initial data", err);
-    }
-
-}).catch((e) => console.error('Failed to connect to DB', e));
-
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
 app.use(express.json());
-
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
-
 app.use(pinoHttp({ logger }));
-
 // Use Routes
 app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
-
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
-
-app.get("/", (req, res) => {
-    res.send("Inside the server");
-});
-
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
