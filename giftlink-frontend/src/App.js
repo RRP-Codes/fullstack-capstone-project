4731a4ef3debb5e0565b211da27d5de93b95cdfa
RRP-Codes <ray.peel@live.com>
2025-12-27 07:12:23 -0500

    Register and login pages

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,25 +1,24 @@
 /*jshint esversion: 8 */
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
 function App() {
-    return (
+  const navigate = useNavigate();
+  return (
         <>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/app" element={<MainPage />} />
-                <Route path="/app/login" element={<LoginPage />} />
-                <Route path="/app/register" element={<RegisterPage />} />
-            </Routes>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
+        </Routes>
         </>
-    );
+  );
 }
-
 export default App;
